7eecf1ebd28c7bdd654abc0d6a56f91157e45a9d
Eric Hugo <105161439+AviciiLove-Github@users.noreply.github.com>
2026-01-30 17:49:47 +0800

    Delete f3d-3.2 directory

--- f3d-3.2/skeleton.py
+++
@@ -1,5 +0,0 @@
-def Skeleton(skeleton):
-    s = ' 用户摆出以下姿势：'+ skeleton + ' 其中l是左，r是右，如leg_twistl表示左腿扭转，leg_twistr表示右腿扭转'
-    with open('Temp-S', 'w', encoding='utf-8') as f:
-        f.write(s)
-    return skeleton
